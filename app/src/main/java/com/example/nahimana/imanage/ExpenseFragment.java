450f2cef2150ab59af76988a3117a0800e9c4eef
nahimanajz <jazzonahimana@gmail.com>
2019-11-19 17:10:34 +0200

    As se , I should be able to add expense

--- app/src/main/java/com/example/nahimana/imanage/ExpenseFragment.java
+++ app/src/main/java/com/example/nahimana/imanage/ExpenseFragment.java
@@ -10,12 +10,30 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.example.nahimana.imanage.helpers.Constants;
+import com.example.nahimana.imanage.helpers.RequestHandler;
+import com.example.nahimana.imanage.helpers.SharedUserData;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class ExpenseFragment extends Fragment implements AdapterView.OnItemSelectedListener {
 
-
+    Spinner spinner;
+    private String selectedCategory;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -33,7 +51,7 @@ public class ExpenseFragment extends Fragment implements AdapterView.OnItemSelec
 try {
 
 
-    Spinner spinner = getView().findViewById(R.id.expenseReason);
+    spinner = getView().findViewById(R.id.expenseReason);
     ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(), R.array.available_expenses, android.R.layout.simple_spinner_item);
     adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
     spinner.setAdapter(adapter);
@@ -43,20 +61,68 @@ try {
   catch (Exception e) {
     e.printStackTrace();
   }
+
     }
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        String text = parent.getItemAtPosition(position).toString();
-        //Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+        selectedCategory = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), selectedCategory, Toast.LENGTH_SHORT).show();
+        try {
+            EditText detail = getView().findViewById(R.id.detail);
+            EditText takMoney = getView().findViewById(R.id.takenMoney);
+            Button spendBtn = getView().findViewById(R.id.saveBtn);
+
+            final String dtl = detail.getText().toString();
+            final String tMny = takMoney.getText().toString();
+
+            spendBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //Toast.makeText(getContext(),"detail: "+dtl + " money"+tMny,Toast.LENGTH_LONG).show();
+                    StringRequest sr = new StringRequest(Request.Method.POST, Constants.EXPENSE_URL, new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            //Toast.makeText(getActivity(),"success",Toast.LENGTH_SHORT).show();
+
+                            try {
+                                JSONObject jo = new JSONObject(response);
+                                Toast.makeText(getActivity(),jo.getString("message"),Toast.LENGTH_LONG).show();
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Toast.makeText(getActivity(),error.getMessage(),Toast.LENGTH_LONG).show();
+
+                        }
+                    }){
+                        @Override
+                        protected Map<String, String> getParams() throws AuthFailureError {
+                            Map<String, String> params = new HashMap<String, String>();
+                            params.put("user_id", SharedUserData.getInstance(getActivity().getApplicationContext()).getUserId());
+                            params.put("category", selectedCategory);
+                             params.put("detail", dtl);
+                             params.put("moneyTaken", tMny);
+                            return params;
+                        }
+                    };
+                    RequestHandler.getInstance(getActivity().getApplicationContext()).addToRequestQueue(sr);
+                }
+            });
+
+        }catch (Exception e) {
+            Toast.makeText(getActivity(),e.getMessage(),Toast.LENGTH_LONG).show();
+        }
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
 
     }
-    public void setExpense()
-    {
 
-    }
+
 }
