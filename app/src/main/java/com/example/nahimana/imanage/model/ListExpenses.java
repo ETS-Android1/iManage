e2d7b16fdf314f8f6761b1c1a5d3ad46351a0180
Janvier <jazzonahimana@gmail.com>
2020-10-02 21:40:00 +0200

    refactor list of debits

--- app/src/main/java/com/example/nahimana/imanage/model/ListExpenses.java
+++ app/src/main/java/com/example/nahimana/imanage/model/ListExpenses.java
@@ -1,4 +1,37 @@
 package com.example.nahimana.imanage.model;
 
 public class ListExpenses {
+        private int _id;
+        private String category;
+        private String description;
+        private int amount;
+        private String date;
+
+    public ListExpenses(int _id, String category, String description, int amount, String date) {
+        this._id = _id;
+        this.category = category;
+        this.description = description;
+        this.amount = amount;
+        this.date = date;
+    }
+
+    public int get_id() {
+        return _id;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public String getDate() {
+        return date;
+    }
 }
