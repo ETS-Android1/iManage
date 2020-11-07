bbeff5c9f02fc8936e4376bb2c31a411e78358e4
nahimanajz <jazzonahimana@gmail.com>
2020-11-07 12:59:11 +0200

    As a user, I should be able to pay a debit

--- app/src/main/java/com/example/nahimana/imanage/helpers/CreditAdapter.java
+++ app/src/main/java/com/example/nahimana/imanage/helpers/CreditAdapter.java
@@ -6,7 +6,9 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.nahimana.imanage.R;
 import com.example.nahimana.imanage.model.ListCredits;
@@ -16,10 +18,12 @@ import java.util.List;
 public class CreditAdapter extends RecyclerView.Adapter<CreditAdapter.CreditViewHolder> {
     private Context context;
     private List<ListCredits> listCredits;
+     private OnCreditClickListener ccl;
 
-    public CreditAdapter(Context context, List<ListCredits> listCredits) {
+    public CreditAdapter(Context context, List<ListCredits> listCredits,OnCreditClickListener ccl ) {
         this.context = context;
         this.listCredits = listCredits;
+        this.ccl = ccl;
     }
 
     @NonNull
@@ -28,10 +32,9 @@ public class CreditAdapter extends RecyclerView.Adapter<CreditAdapter.CreditView
         View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.fragment_credited_tab, viewGroup, false);
         return new CreditViewHolder(view);
     }
-
     @Override
-    public void onBindViewHolder(@NonNull CreditViewHolder cvh, int index) {
-        ListCredits listCredit = listCredits.get(index);
+    public void onBindViewHolder(@NonNull final CreditViewHolder cvh, int index) {
+        final ListCredits listCredit = listCredits.get(index);
         cvh.creditAmount.setText(listCredit.getAmount());
         cvh.creditId.setText(listCredit.get_id());
         cvh.creditorName.setText(listCredit.getCreditor());
@@ -40,7 +43,12 @@ public class CreditAdapter extends RecyclerView.Adapter<CreditAdapter.CreditView
         cvh.creditPaymentDate.setText(listCredit.getPaymentDate());
         cvh.payedAmount.setText("CurrentPayment: "+listCredit.getPayedAmount());
         cvh.remainingDays.setText(listCredit.getRemainingDays());
-
+        cvh.payCreditButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    ccl.onCreditClick(listCredit, cvh.payCreditAmount.getText().toString());
+            }
+        });
     }
 
     @Override
@@ -49,7 +57,8 @@ public class CreditAdapter extends RecyclerView.Adapter<CreditAdapter.CreditView
     }
 
     public class CreditViewHolder extends RecyclerView.ViewHolder {
-        public TextView creditId, creditorName, creditorPhone, creditAmount, creditPaymentDate, creditDueDate, payedAmount, remainingDays;
+        public TextView creditId, creditorName, creditorPhone, creditAmount, creditPaymentDate, creditDueDate, payedAmount, remainingDays, payCreditAmount;
+        public Button payCreditButton;
 
         public CreditViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -61,9 +70,11 @@ public class CreditAdapter extends RecyclerView.Adapter<CreditAdapter.CreditView
             creditorPhone = itemView.findViewById(R.id.creditorPhone);
             payedAmount = itemView.findViewById(R.id.payedAmount);
             remainingDays = itemView.findViewById(R.id.remainingDays);
-
-
-
+            payCreditAmount = itemView.findViewById(R.id.payCreditAmount);
+            payCreditButton = itemView.findViewById(R.id.payCreditBtn);
         }
+    }
+    public interface OnCreditClickListener {
+        void onCreditClick(ListCredits lc, String amount);
     }
 }
--- app/src/main/java/com/example/nahimana/imanage/model/ListDebits.java
+++ app/src/main/java/com/example/nahimana/imanage/model/ListDebits.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.widget.Toast;
 
 public class ListDebits {
+    private String id;
     private String names;
     private String phone;
     private String amount;
@@ -12,7 +13,8 @@ public class ListDebits {
     private String payedAmount;
     private  String remainingDays;
 
-    public ListDebits(String names, String phone, String amount, String paymentDate, String dueDate, String payedAmount, String remainingDays) {
+    public ListDebits(String id, String names, String phone, String amount, String paymentDate, String dueDate, String payedAmount, String remainingDays) {
+        this.id = id;
         this.names = names;
         this.phone = phone;
         this.amount = amount;
@@ -49,4 +51,11 @@ public class ListDebits {
         return remainingDays;
     }
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 }
