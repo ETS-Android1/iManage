0d105228096c66cbd7c716c6a0e25e4b87c8d95a
nahimanajz <jazzonahimana@gmail.com>
2021-01-13 19:38:10 +0200

    add filter on credit list

--- app/src/main/java/com/example/nahimana/imanage/DepositActivity.java
+++ app/src/main/java/com/example/nahimana/imanage/DepositActivity.java
@@ -65,7 +65,6 @@ public class DepositActivity extends AppCompatActivity {
                     }, new Response.ErrorListener() {
                         @Override
                         public void onErrorResponse(VolleyError error) {
-
                             error.printStackTrace();
                         }
                     })
